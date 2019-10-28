058d8e161b3c55c598261ca7f1bf802dc6dbc9e4
Guillaume Deschamps <guillaumedeschamps75@gmail.com>
2019-10-28 15:30:39 +0100

    fusion head header créa sign-up.php

--- private/sign-up.php
+++ private/sign-up.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ACCUEIL - BIENVENUE SUR LE SITE DE LOCATION JET SKI</title>
+    <title>SIGN-UP - VOUS CONNECTER</title>
+    <!-- PRESENTATION -->
 <?php
-    require('inc/head.php');
+    require('../inc/head.php')
+
 ?>
 <!-- CONTENU PRINCIPAL -->
-<main>
-<p>Un autre paragraphe</p>
+<main class="container">
+<h1>CONNEXION</h1>
+<form action="">
+
+</form>
 </main>
 <!-- FIN CONTENU PRINCIPAL -->
 <?php    
-    require('inc/footer.php')
+    require('../inc/footer.php')
 ?>
