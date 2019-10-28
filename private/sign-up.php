25c0e90a92212019fa5a32fadda6a430a645aba7
Guillaume Deschamps <guillaumedeschamps75@gmail.com>
2019-10-28 16:36:03 +0100

    crea des pages et formulaires signs

--- private/sign-up.php
+++ private/sign-up.php
@@ -12,12 +12,89 @@
 ?>
 <!-- CONTENU PRINCIPAL -->
 <main class="container">
-<h1>CONNEXION</h1>
-<form action="">
+  <h1>ENREGISTREMENT</h1>
+  <form>
+    
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputNom">Nom</label>
+        <input type="text" class="form-control" id="inputNom" placeholder="Nom">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPrenom">Prénom</label>
+        <input type="text" class="form-control" id="inputPrenom" placeholder="Prénom">
+      </div>
+    </div>
 
-</form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail">Email</label>
+        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword">Mot de passe</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="inputAddress">Addresse 1</label>
+      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    </div>
+    <div class="form-group">
+      <label for="inputAddress2">Addresse 2</label>
+      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputCity">Ville</label>
+        <input type="text" class="form-control" id="inputCity">
+      </div>
+      
+      <div class="form-group col-md-6">
+        <label for="inputZip">Code postal</label>
+        <input type="text" class="form-control" id="inputZip">
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="dateBirth">Date de naissance</label>
+        <input type="date" class="form-control" id="dateBirth">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputTel">Téléphone</label>
+        <input type="tel" class="form-control" id="inputTel">
+      </div>
+    </div>
+
+    <div class="form-row">
+
+      <div class="form-group col-md-6">
+        <label for="inputPermis">Permis côtier</label>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+          <label class="form-check-label" for="inlineCheckbox1">oui</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+          <label class="form-check-label" for="inlineCheckbox2">Non</label>
+        </div>
+      </div>
+      <?php /* si la case oui est cochée, demander le n° de permis  */ ?>
+      <div class="form-group col-md-6">
+        <label for="inputNpermis">N° permis cotier</label>
+        <input type="text" class="form-control" id="inputNpermis">
+      </div>
+
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+
+  </form>
 </main>
 <!-- FIN CONTENU PRINCIPAL -->
 <?php    
-    require('../inc/footer.php')
+    require('../inc/footer.php');
 ?>
